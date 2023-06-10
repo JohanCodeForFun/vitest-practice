9bcfe14dc149f853e5e7ba6a2edd118643e34643
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-10 20:32:08 +0200

    Write tests to reverseString and pass

--- src/components/__tests__/hello.test.js
+++ src/components/__tests__/hello.test.js
@@ -3,7 +3,7 @@ import { test, expect } from 'vitest'
 // Assignment
 // Write tests for the following, and then make the tests pass!
 // DONE - A capitalize function that takes a string and returns it with the first character capitalized.
-// A reverseString function that takes a string and returns it reversed.
+// DONE - A reverseString function that takes a string and returns it reversed.
 // A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
 // A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”. Read more about how a Caesar cipher works on this website.
 
@@ -33,4 +33,20 @@ test('capitalizeString Adam', () => {
 })
 test('capitalizeString Adam', () => {
   expect(capitalizeString('bob')).toEqual('Bob')
+})
+
+// A reverseString function that takes a string and returns it reversed.
+function reverseString(string) {
+  const reversedString = [...string].reverse().join('')
+  return reversedString
+}
+
+test('reverse string Hello', () => {
+  expect(reverseString('Hello')).toEqual('olleH')
+})
+test('reverse string World', () => {
+  expect(reverseString('World')).toEqual('dlroW')
+})
+test('reverse string racecar', () => {
+  expect(reverseString('racecar')).toEqual('racecar')
 })
