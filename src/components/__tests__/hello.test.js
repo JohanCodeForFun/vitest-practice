92ebd6b0dd6538503dfa05ae65728af46309dac8
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-10 20:41:38 +0200

    write tests and function for simple calculator

--- src/components/__tests__/hello.test.js
+++ src/components/__tests__/hello.test.js
@@ -4,7 +4,7 @@ import { test, expect } from 'vitest'
 // Write tests for the following, and then make the tests pass!
 // DONE - A capitalize function that takes a string and returns it with the first character capitalized.
 // DONE - A reverseString function that takes a string and returns it reversed.
-// A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+// DONE - A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
 // A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”. Read more about how a Caesar cipher works on this website.
 
 // sum function
@@ -49,4 +49,34 @@ test('reverse string World', () => {
 })
 test('reverse string racecar', () => {
   expect(reverseString('racecar')).toEqual('racecar')
+})
+
+
+// A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+function calculator(a, b, operation) {
+    if (operation === 'add') {
+      return a + b;
+    }
+    else if (operation === 'sub') {
+      return a - b;
+    }
+    else if (operation === 'multiply') {
+      return a * b;
+    }
+    else if (operation === 'divide') {
+      return a / b;
+    }
+}
+
+test('1 + 2', () => {
+  expect(calculator(1,2,'add')).toEqual(3)
+})
+test('1 - 2', () => {
+  expect(calculator(1,2,'sub')).toEqual(-1)
+})
+test('1 * 2', () => {
+  expect(calculator(1,2,'multiply')).toEqual(2)
+})
+test('1 / 2', () => {
+  expect(calculator(1,2,'divide')).toEqual(0.5)
 })
